8db73c1ebc34c343fa0e0e091c43e2b3b90805c9
段杰 <sdir@users.noreply.github.com>
2019-05-07 10:21:57 +0800

    generate a QR Code with half blocks (#10)
    close #4

--- jike/qr.go
+++ jike/qr.go
@@ -1,18 +1,22 @@
 package jike
 
 import (
-	"github.com/mdp/qrterminal"
 	"os"
+
+	"github.com/mdp/qrterminal"
 )
 
 func GenerateQRCode(uuid string) {
 	url := `jike://page.jk/web?url=https%3A%2F%2Fruguoapp.com%2Faccount%2Fscan%3Fuuid%3D` + uuid + `&displayHeader=false&displayFooter=false`
 	config := qrterminal.Config{
-		Level:     qrterminal.L,
-		Writer:    os.Stdout,
-		BlackChar: qrterminal.BLACK,
-		WhiteChar: qrterminal.WHITE,
-		QuietZone: 2,
+		Level:          qrterminal.L,
+		Writer:         os.Stdout,
+		HalfBlocks:     true,
+		BlackChar:      qrterminal.BLACK_BLACK,
+		WhiteBlackChar: qrterminal.WHITE_BLACK,
+		WhiteChar:      qrterminal.WHITE_WHITE,
+		BlackWhiteChar: qrterminal.BLACK_WHITE,
+		QuietZone:      2,
 	}
 	qrterminal.GenerateWithConfig(url, config)
 }
